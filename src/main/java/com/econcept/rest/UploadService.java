cfa58f63ec01960d74194904f527bc9fda8610a8
DANILKO <danilko@danilko-System-Product-Name.(none)>
2013-05-19 00:00:46 -0700

    Update comments, clean up unnecessary codes, correct download files to reflect correct names

--- src/main/java/com/econcept/rest/UploadService.java
+++ src/main/java/com/econcept/rest/UploadService.java
@@ -1,3 +1,34 @@
+/**
+ * 
+ * The MIT License (MIT)
+ * 
+ * Copyright (c) 2013 Kai-Ting (Danil) Ko
+ * 
+ * Permission is hereby granted, free of charge, 
+ * to any person obtaining a copy of this software 
+ * and associated documentation files (the "Software"), 
+ * to deal in the Software without restriction, including 
+ * without limitation the rights to use, copy, modify, 
+ * merge, publish, distribute, sublicense, and/or sell 
+ * copies of the Software, and to permit persons to whom 
+ * the Software is furnished to do so, subject to the 
+ * following conditions:
+ * 
+ * The above copyright notice and this permission notice 
+ * shall be included in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY 
+ * OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED 
+ * TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR 
+ * PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS 
+ * OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, 
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE 
+ * USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * 
+ */
+
+
 package com.econcept.rest;
 
 import java.io.File;
@@ -22,150 +53,290 @@ import org.codehaus.jettison.json.JSONObject;
 import com.sun.jersey.core.header.FormDataContentDisposition;
 import com.sun.jersey.multipart.FormDataParam;
 
-
-
+/**
+ * 
+ * @author Kai - Ting (Danil) Ko
+ * <h1>UploadService</h1>
+ * <p>To provide rest end points for upload, delete, get file, get file lists according to user input</p>
+ *
+ */
 @Path("/")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
-public class UploadService {
-	
+public class UploadService
+{
+
+	/**
+	 * <h1>getFileList</h1>
+	 * <p>Get the all files that owned by the user according to the user information</p>
+	 * 
+	 * @param pMessage <p>The JSON message string that contains user information, 
+	 *            and the file name for the file to retrieve</p>
+	 *           <p>JSON parameters and their values</p>
+	 *           <p>user type = string</p>
+	 *           <p>The username for the user</p>
+	 *           <p>parameter type = string</p>
+	 *           <p>The file name for the file to be deleted</p>
+	 * @return String in JSON format
+	 *           <p>JSON parameters and their values</p>
+	 *           <p>status type = string</p>
+	 *           <p>The username for the user</p>
+	 *           <p>statusmessage type = string/string array</p>
+	 *           <p>The status message to log detail information about operation</p>
+	 */
 	@POST
 	@Path("/getfilelist")
-	public String getFileList(String pMessage) {
-		try {
+	public String getFileList(String pMessage)
+	{
+		try
+		{
 			JSONObject lObject = new JSONObject(pMessage);
-			
+
 			String lUser = lObject.getString("user");
-			
+
+			// Convert data dir to UNIX like path to eliminate possible errors
 			File lFolder = new File(System.getenv("DATA_DIR").toString()
-					.replace("\\", "/") + "/" + lUser);
+					.replace("\\", "/")
+					+ "/" + lUser);
+			// Retrieve all files from file lists
 			File[] lFileList = lFolder.listFiles();
+
+			// Start build JSON array for file list
 			StringBuilder lBuilder = new StringBuilder();
 			lBuilder.append("[");
-			for(int lIndex = 0; lIndex < lFileList.length; lIndex++)
+
+			// Only go through file list if file list itself is not null
+			if (lFileList != null)
 			{
-				lBuilder.append("\"");
-				lBuilder.append(lFileList[lIndex].getName());
-				lBuilder.append("\"");
-				
-				if(lIndex + 1 < lFileList.length)
+				for (int lIndex = 0; lIndex < lFileList.length; lIndex++)
 				{
-					lBuilder.append(",");
-				}  // if
-			}  // for
+					// Add double quotes to surround each file name
+					lBuilder.append("\"");
+					lBuilder.append(lFileList[lIndex].getName());
+					lBuilder.append("\"");
+
+					// If next element is available, add comma
+					if (lIndex + 1 < lFileList.length)
+					{
+						lBuilder.append(",");
+					} // if
+				} // for
+			} // if
 			lBuilder.append("]");
 			
 			return "{\"status\": \"success\",\"statusmessage\": "
-			+ lBuilder.toString() + "}";
-			
-		} catch (Exception pException) {
+					+ lBuilder.toString() + "}";
+
+		} // try
+		catch (Exception pException)
+		{
 			pException.printStackTrace();
 			return "{\"status\": \"error\",\"statusmessage\": \""
-			+ pException.toString().replace("\\", "/")
-					.replace("\"", "\\\"") + "\"}";
+					+ pException.toString().replace("\\", "/")
+							.replace("\"", "\\\"") + "\"}";
 		} // catch
-	}
-	
+	} // String getFileList
+
+	/**
+	 * <h1>getFile</h1>
+	 * <p>Retrieve the particular file according to user information</p>
+	 * 
+	 * @param @QueryParam("user") String pUser The user name for the user
+	 * @param @QueryParam("filename") String pFileName The file name for the user         
+	 * @return HTTP Response that contains the downloaded object
+	 */
 	@GET
 	@Path("/getfile")
 	@Produces("application/data")
-	public Response getFile(
-			@QueryParam("user") String pUser,
-			@QueryParam("filename") String pFileName
-			) {
-		try {
-			
+	public Response getFile(@QueryParam("user") String pUser,
+			@QueryParam("filename") String pFileName)
+	{
+		try
+		{
+
+			// Retrieve the file through user information, convert to UNIX format to eliminate
+			// possible errors
 			File lFile = new File(System.getenv("DATA_DIR").toString()
-					.replace("\\", "/") + "/" + pUser + "/"
-					+ "/" + pFileName);
-			
+					.replace("\\", "/")
+					+ "/" + pUser + "/" + "/" + pFileName);
+
+			// Build the response and add file as part of the response
 			ResponseBuilder lResponse = Response.ok((Object) lFile);
-			lResponse.header("Context-Disposition", "attachement; filename=save.txt");
+			lResponse.header("Content-Disposition",
+					"attachment; filename=\"" + pFileName + "\"");
+			
 			return lResponse.build();
-		} catch (Exception pException) {
+		}  // try
+		catch (Exception pException)
+		{
 			pException.printStackTrace();
 			return Response.noContent().build();
 		} // catch
-	}
+	}  // Response getFile
 
+	/**
+	 * 
+	 * <p>deleteFile</p>
+	 * <p>Delete particular file owned by the user</p>
+	 * 
+	 * @param pMessage <p>The JSON message string that contains user information, 
+	 *            and the file name for the file to retrieve</p>
+	 *           <p>JSON parameters and their values</p>
+	 *           <p>user type = string</p>
+	 *           <p>The username for the user</p>
+	 *           <p>parameter type = string</p>
+	 *           <p>The file name for the file to be deleted</p>
+	 * @return String in JSON format
+	 *           <p>JSON parameters and their values</p>
+	 *           <p>status type = string</p>
+	 *           <p>The username for the user</p>
+	 *           <p>statusmessage type = string/string array</p>
+	 *           <p>The status message to log detail information about operation</p>
+	 */
 	@POST
 	@Path("/deletefile")
-	public String deleteFile(String pMessage) {
-		try {
+	public String deleteFile(String pMessage)
+	{
+		try
+		{
+			// Convert string into a JSON string
 			JSONObject lObject = new JSONObject(pMessage);
-			
+
+			// Retrieve username
 			String lUser = lObject.getString("user");
-			
+
+			// Set the file path, convert to UNIX format
 			String lFileName = lObject.getString("parameter");
 			File lFile = new File(System.getenv("DATA_DIR").toString()
-					.replace("\\", "/") + "/" + lUser + "/"
-					+ "/" + lFileName);
+					.replace("\\", "/")
+					+ "/" + lUser + "/" + "/" + lFileName);
 
+			// Remove the file
 			lFile.delete();
 
-			return "{\"status\": \"success\",\"statusmessage\": \""
-					+ lFileName + " Removed Successfuly\"}";
-		} catch (Exception pException) {
+			return "{\"status\": \"success\",\"statusmessage\": \"" + lFileName
+					+ " Removed Successfuly\"}";
+		}  // catch 
+		catch (Exception pException)
+		{
 			pException.printStackTrace();
 			return "{\"status\": \"error\",\"statusmessage\": \""
 					+ pException.toString().replace("\\", "/")
 							.replace("\"", "\\\"") + "\"}";
 		} // catch
 	}
-	
-	
+
+	/**
+	 * 
+	 * <p>uploadwithuri</p>
+	 * <p>Upload a file using user inputed uri and owned by the user</p>
+	 * 
+	 * @param pMessage <p>The JSON message string that contains user information, 
+	 *            and the file name for the file to retrieve</p>
+	 *           <p>JSON parameters and their values</p>
+	 *           <p>user type = string</p>
+	 *           <p>The username for the user</p>
+	 *           <p>parameter type = string</p>
+	 *           <p>The uri for the file to be downloaded</p>
+	 * @return String in JSON format
+	 *           <p>JSON parameters and their values</p>
+	 *           <p>status type = string</p>
+	 *           <p>The username for the user</p>
+	 *           <p>statusmessage type = string/string array</p>
+	 *           <p>The status message to log detail information about operation</p>
+	 */
 	@POST
 	@Path("/uploadwithuri")
-	public String uploadWithURI(String pMessage) {
-		try {
+	public String uploadWithURI(String pMessage)
+	{
+		try
+		{
+			// Convert to JSON object
 			JSONObject lObject = new JSONObject(pMessage);
-			
+
 			String lUser = lObject.getString("user");
+
+			// Analysis uri to try to find file name
+			String[] lURLSplit = lObject.getString("parameter").split("/");
+			String lFileName = lURLSplit[lURLSplit.length - 1];
 			
-			String [] lURLSplit = lObject.getString("parameter").split("/");
-			String lFileName = lURLSplit [lURLSplit.length - 1];
+			// Set the output file and conver to UNIX styles
 			File lFile = new File(System.getenv("DATA_DIR").toString()
-					.replace("\\", "/") + "/" + lUser + "/"
-					+ "/" + lFileName);
+					.replace("\\", "/")
+					+ "/" + lUser + "/" + "/" + lFileName);
+			
+			// Set the URL to download files from
 			URL lURL = new URL(lObject.getString("parameter"));
 			FileUtils.copyURLToFile(lURL, lFile);
 
+			// Return the operation status as JSON format
 			return "{\"status\": \"success\",\"statusmessage\": \""
 					+ lFile.getAbsolutePath().replace("\\", "/") + "\"}";
-		} catch (Exception pException) {
+		}  // try
+		catch (Exception pException)
+		{
 			pException.printStackTrace();
 			return "{\"status\": \"error\",\"statusmessage\": \""
 					+ pException.toString().replace("\\", "/")
 							.replace("\"", "\\\"") + "\"}";
 		} // catch
-	}
+	}  // String uploadWithURI
 
+	/**
+	 * 
+	 * <p>uploadwithuri</p>
+	 * <p>Upload a file using user inputed uri and owned by the user</p>
+	 * 
+	 * @param pMessage <p>The JSON message string that contains user information, 
+	 *            and the file name for the file to retrieve</p>
+	 *           <p>JSON parameters and their values</p>
+	 *           <p>user type = string</p>
+	 *           <p>The username for the user</p>
+	 *           <p>parameter type = string</p>
+	 *           <p>The uri for the file to be downloaded</p>
+	 * @return String in JSON format
+	 *           <p>JSON parameters and their values</p>
+	 *           <p>status type = string</p>
+	 *           <p>The username for the user</p>
+	 *           <p>statusmessage type = string/string array</p>
+	 *           <p>The status message to log detail information about operation</p>
+	 */
 	@POST
 	@Path("/upload")
 	@Consumes(MediaType.MULTIPART_FORM_DATA)
 	public String upload(
 			@FormDataParam("file") InputStream pUploadedInputStream,
-			@FormDataParam("file") FormDataContentDisposition pFileDetail) {
+			@FormDataParam("file") FormDataContentDisposition pFileDetail)
+	{
+		// User is admin now, need to change to be able to read user info from form
 		String lUser = "admin";
+		// Set the file output path and convert to UNIX format
 		File lFile = new File(System.getenv("DATA_DIR").toString()
-				.replace("\\", "/") + "/" + lUser + "/"
-				+ "/" + pFileDetail.getFileName());
-		try {
+				.replace("\\", "/")
+				+ "/" + lUser + "/" + "/" + pFileDetail.getFileName());
+		try
+		{
+			// Retrieve output stream through multipart data
 			OutputStream lOutputStream = new FileOutputStream(lFile);
 			int lRead = 0;
+			// Set the upload threshold to 1 KB for every read
 			byte[] lBytes = new byte[1024];
-			while ((lRead = pUploadedInputStream.read(lBytes)) != -1) {
+			// Write while read from form data
+			while ((lRead = pUploadedInputStream.read(lBytes)) != -1)
+			{
 				lOutputStream.write(lBytes, 0, lRead);
 			} // while
 
 			lOutputStream.flush();
 			lOutputStream.close();
 			return "{\"status\": \"" + "success" + "\"}";
-		}  // try
-		catch (Exception pException) {
+		} // try
+		catch (Exception pException)
+		{
+			pException.printStackTrace();
 			return "{\"status\": \"error\",\"statusmessage\": \""
 					+ pException.toString().replace("\\", "/")
 							.replace("\"", "\\\"") + "\"}";
-		}  // catch
-	}
-}
+		} // catch
+	}  // try
+}  // class UploadService
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -36,46 +36,74 @@
         </script>
   <script src="js/jquery.form.js"></script>
   <script type="text/javascript">
-    $(document).ready(function () {
+  //
+  // document ready
+  // Initialize functions once document loading is complete
+  //
+    $(document).ready(function () 
+    	{
+    	// Retreive file list
     	getFileList();
+    	
+    	// Get progress div
         var lProgress = $('#progress');
+        
+    	// Initilaze AJAX upload form
         $('#file_upload_form').ajaxForm({
+        	// Initialize status
             beforeSend: function () {
                 var lPercentValue = '0%';
                 lProgress.html(lPercentValue);
             },  // beforeSend: function
+            // Update progress
             uploadProgress: function (pEvent, pPosition, pTotal, pPercentComplete) {
-                var lPercentValue = pPercentComplete + '%';
+                // Update percentage complete
+            	var lPercentValue = pPercentComplete + '%';
                 lProgress.html(lPercentValue);
             },  // beforeSend: function
-            complete: function (pXHR) {
+            complete: function (pXHR) 
+            {
+            	// Analysis message as a JSON object from responseText
             	var lJSONObject = jQuery.parseJSON(pXHR.responseText)
                 $('#result').append('<p>' + lJSONObject.status + '</p>');
                 $('#result').append('<p>' + lJSONObject.statusmessage + '</p>');
                 
+                // Update progress and file list once update completes
                 if(lJSONObject.status=='success')
                 {
                 	lProgress.html('100% Complete');
                 	getFileList();
                 }  // if
+                
             	$('#result').html('');
             }  // complete: function
         });  // $('form').ajaxForm
     });  // $(document).ready(function(){})
     
+    //
+    // updateWithURI
+    // Compute user information and file uri as a JSON message and send to server
+    // to download file
+    // Input: None
+    // Output: None
+    //
     function updateWithURI() {
+    	// Compute JSON data
         var lJSONData = "{";
-        lJSONData = lJSONData + "\"parameter\":\"" + $("#helloinput").val() + "\",";
+        lJSONData = lJSONData + "\"parameter\":\"" + $("#txtUploadFileURI").val() + "\",";
         lJSONData = lJSONData + "\"user\":\"admin\"";
         lJSONData = lJSONData + "}";
 
+        // AJAX call
         $.ajax({
             url: "rest/uploadwithuri",
             type: "POST",
             data: lJSONData,
             dataType: "json",
             contentType: "application/json; charset=utf-8",
-            success: function (pData) {
+            success: function (pData) 
+            {
+            	// Print out status and message
             	$('#result').html('');
                 $('#result').append('<p>' + pData.status + '</p>');
                 $('#result').append('<p>' + pData.statusmessage + '</p>');
@@ -89,7 +117,14 @@
         });  // $.ajax
     }  // function updateWithURI
     
+    //
+    // getFileList
+    // Retrieve file list
+    // Compute user information as JSON object
+    // Input: None
+    // Output: None
     function getFileList() {
+    	// Compute JSON information
         var lJSONData = "{";
         lJSONData = lJSONData + "\"user\":\"admin\"";
         lJSONData = lJSONData + "}";
@@ -104,21 +139,31 @@
             	$('#result').html('');
                 $('#result').append('<p>' + pData.status + '</p>');
                 
+                // If the operation status is error, display it and end the
+                // current function
                 if(pData.status == 'error')
                 {
                     $('#result').append('<p>' + pData.statusmessage + '</p>');
                     return;
                 }  // if
+                
+                // Clear the file list to ensure newest list is updated
                 $('#filelist').html('');
+                
+                // Get the file list length
                 var lFileListLength = pData.statusmessage.length;
                 
+                // Only need to go through file list if there is one or more,
+                // else print the message to show no file and end the function
                 if(lFileListLength == 0)
                 	{
                 	 $('#filelist').append('There is no file exist for this user. Upload some file through URI or Web based upload.');
                 	 return;
                 	}  // if
                 	
+                // Prepare table to display file list
 				var lTableHTMLString = '<table class="table table-striped">';
+				// Table Header
 				lTableHTMLString = lTableHTMLString + '<thead>';
 				lTableHTMLString = lTableHTMLString + '<tr>';
             	lTableHTMLString = lTableHTMLString + '<th>#</td>';
@@ -127,17 +172,22 @@
     			lTableHTMLString = lTableHTMLString + '</tr>';
     			lTableHTMLString = lTableHTMLString + '</thead>';
 				
+    			// Loop through the file list and concate file into table row
                 for(var lFileIndex = 0; lFileIndex < lFileListLength; lFileIndex++)
                 {
                 	lTableHTMLString = lTableHTMLString + '<tr>';
+                	// File Name
                 	lTableHTMLString = lTableHTMLString + '<td>' +  (lFileIndex + 1) + '</td>';
                 	lTableHTMLString = lTableHTMLString + '<td><a href="rest/getfile?user=admin&filename=' + pData.statusmessage [lFileIndex] + '">' + pData.statusmessage [lFileIndex] + '</a></td>';
+                	// Delete button for the particular file
                 	lTableHTMLString = lTableHTMLString + '<td><input class="btn" type="button" onclick="deleteFile(\'' + pData.statusmessage [lFileIndex]  + '\');" value="Delete"/></td>';
         			lTableHTMLString = lTableHTMLString + '</tr>';
                 }  // for
-                lTableHTMLString = lTableHTMLString + '</table>';
-                $('#filelist').append(lTableHTMLString);
                 
+                lTableHTMLString = lTableHTMLString + '</table>';
+                
+            	// Add the table into the file list div
+                $('#filelist').append(lTableHTMLString);
             }  // success: function(pData)
             ,
             error: function (xhr, status, thrown) {
@@ -147,12 +197,21 @@
         });  // $.ajax
     }  // function getFileList
     
+     //
+     // deleteFile
+     // Compute the JSON message to send to server to delete the file
+     // for the particular user
+     // Input: pFileName the file to be deleted
+     // OUtput: None
+     //
      function deleteFile(pFileName) {
-        var lJSONData = "{";
+         // Compute JSON data
+    	 var lJSONData = "{";
         lJSONData = lJSONData + "\"parameter\":\"" + pFileName + "\",";
         lJSONData = lJSONData + "\"user\":\"admin\"";
         lJSONData = lJSONData + "}";
 
+        // Perform AJAX Call
         $.ajax({
             url: "rest/deletefile",
             type: "POST",
@@ -256,7 +315,7 @@ THE SOFTWARE.</p>
                 <div class="span4">
                     <h2>Upload with URI</h2>
                     <p>Input an URI and let Server download it for you!</p>
-                    <p><input type="text" id="helloinput" /></p>
+                    <p><input type="text" id="txtUploadFileURI" /></p>
                     <p><input class="btn" type="button" value="Update With URI" id="btnUpdateWithURI" onclick="updateWithURI();"/></p>
                </div>
                 <div class="span4">
@@ -275,7 +334,7 @@ THE SOFTWARE.</p>
             <hr>
 
             <footer>
-                <p>&copy; Company 2012</p>
+                <p>&copy; ECONCEPT STORAGE 2013</p>
             </footer>
 
         </div> <!-- /container -->
