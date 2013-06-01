70c105d5a96b9a0763090eff4a3003b7d58f8935
DanilKo <danilko@danilko-System-Product-Name.(none)>
2013-06-01 16:16:46 -0700

    Add database schema

---
+++ econcept_ods.sql
@@ -0,0 +1,13 @@
+﻿-- Drop existing table if one exist
+DROP TABLE IF EXISTS account;
+
+-- Create Table account to store user account information
+CREATE TABLE account
+(
+accountid VARCHAR (30) NOT NULL,
+firstname VARCHAR (30) NOT NULL,
+lastname VARCHAR (30) NOT NULL,
+accountpassword VARCHAR (30) NOT NULL,
+email VARCHAR(60) NOT NULL,
+accountstatus VARCHAR(30) NOT NULL
+);
